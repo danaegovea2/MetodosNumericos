7f99282cf54d07d12b29c80be4e61fbcac8340a6
danaegovea2 <160983539+danaegovea2@users.noreply.github.com>
2024-04-06 13:44:38 -0600

    Update __init__.py

--- MetodosRaices/__init__.py
+++ MetodosRaices/__init__.py
@@ -1,7 +1,7 @@
-from .metodos_num import biseccion
-from .metodos_num import newton
-from .metodos_num import itera
-from .metodos_num import secante
-from .metodos_num import secante2
+from .MetodosRaices.metodos_num import biseccion
+from .MetodosRaices.metodos_num import newton
+from .MetodosRaices.metodos_num import itera
+from .MetodosRaices.metodos_num import secante
+from .MetodosRaices.metodos_num import secante2
 
 __all__= ['biseccion', 'newton', 'itera', 'secante', 'secante2']
