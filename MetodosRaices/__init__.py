ed2ff11a3e44e8d0948ca6c25ff4f2807cb6d4b3
danaegovea2 <160983539+danaegovea2@users.noreply.github.com>
2024-04-06 13:51:32 -0600

    Delete MetodosRaices/__init__.py

--- MetodosRaices/__init__.py
+++
@@ -1,7 +0,0 @@
-from .MetodosRaices.metodos_num import biseccion
-from .MetodosRaices.metodos_num import newton
-from .MetodosRaices.metodos_num import itera
-from .MetodosRaices.metodos_num import secante
-from .MetodosRaices.metodos_num import secante2
-
-__all__= ['biseccion', 'newton', 'itera', 'secante', 'secante2']
