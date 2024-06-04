cb56308491ba00a30facbcd20140cd6103b354de
danaegovea2 <160983539+danaegovea2@users.noreply.github.com>
2024-06-04 16:01:21 -0600

    Update edoeuleradelante.py

--- EDOS/edoeuleradelante.py
+++ EDOS/edoeuleradelante.py
@@ -11,17 +11,3 @@ def eulerforw(x0, xf, y0, n, func):
     for i in range(1, n):
         y1[i] = deltax * func(x1[i-1], y1[i-1]) + y1[i-1] # fórmula
     return x1, y1
-
-# Definir los valores iniciales y parámetros
-x0 = 0  # Valor inicial de x
-xf = 1  # Valor final de x
-y0 = 1  # Valor inicial de y
-n = 100  # Número de puntos
-func = lambda x, y: x * y  # Función a integrar
-x1, y1 = eulerforw(x0, xf, y0, n, func)
-# print(eulerforw)
-plt.plot(x1, y1)
-plt.xlabel('Valores de x')
-plt.ylabel('Valor de y')
-plt.title('Método de Euler hacia adelante')
-plt.show()
