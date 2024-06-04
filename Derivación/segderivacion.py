4e147911d25aadec3e041c95852cda35ed95f0ba
danaegovea2 <160983539+danaegovea2@users.noreply.github.com>
2024-06-04 16:00:37 -0600

    Update segderivacion.py

--- Derivación/segderivacion.py
+++ Derivación/segderivacion.py
@@ -6,14 +6,6 @@ def deriv2(f, a, b, n):
     dx = (b - a)/(n - 1)
     y = f(x)
     yp = np.zeros_like(x)
-    for i in range(n):
-        if i==0:
-            yp[i] = (y[i + 1] - y[i]) / dx # derivada hacia adelante
-        elif i== n - 1:
-            yp[i] = (y[i] - y[i - 1])/dx # derivada hacia atrás
-        else:
-            yp[i]=(y[i + 1] - y[i - 1])/ (2*dx) # derivada central
-
     ypp = np.zeroslike(x)
     # fórmulas de segunda derivada 
     for i in range(n):
